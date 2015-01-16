7741b1f015a3f8c2f287bc0a629db3ed0d8f4716
Sleiman Sleiman <sleiman@outlook.com>
2015-01-16 12:57:31 +0200

    adress book tables migrations

---
+++ database/migrations/2015_01_16_105017_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('phonePrimary');
+            $table->string('phoneSecondary');
+            $table->string('location');
+            $table->decimal('lat', 18, 14);
+            $table->decimal('long', 18, 14);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contacts');
+    }
+
+}
