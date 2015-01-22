fcf26465dae47f5e04130a17f9f7dfc0d49a5fba
Sleiman Sleiman <sleiman@outlook.com>
2015-01-22 10:22:02 +0200

    Auth controller update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,22 +1,40 @@
 <?php namespace LRC\Http\Controllers\Auth;
 
 use LRC\Http\Controllers\Controller;
+use Illuminate\Auth\Guard;
+use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
 
-	use AuthenticatesAndRegistersUsers;
+    use AuthenticatesAndRegistersUsers;
+
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @param \Illuminate\Contracts\Auth\Guard|Guard $auth
+     * @param \Illuminate\Contracts\Auth\Registrar|Registrar $registrar
+     */
+    public function __construct(Guard $auth, Registrar $registrar)
+    {
+        $this->auth      = $auth;
+        $this->registrar = $registrar;
+
+        $this->middleware('auth', ['only' => ['postRegister','getRegister','getLogout']]);
+
+    }
 
 
 }
